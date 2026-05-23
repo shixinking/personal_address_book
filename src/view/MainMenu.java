df0fbe334bb051e5f5fc8518b7a8ca6333ee9e58
start <2105493561@qq.com>
2026-05-23 17:01:53 +0800

    完成文字交互

---
+++ src/view/MainMenu.java
@@ -0,0 +1,24 @@
+package view;
+
+public class MainMenu {
+    public static void show()
+    {
+        System.out.println("---------------------------------");
+        System.out.println("0.展示通讯录");
+        System.out.println("1.查询联系人");
+        System.out.println("2.删除联系人");
+        System.out.println("3.添加联系人");
+        System.out.println("4.更新联系人信息");
+        System.out.println("5.重置通讯录");
+        System.out.println("6.退出系统");
+        System.out.println("---------------------------------");
+    }
+
+    public static void exit()
+    {
+        System.out.println("欢迎下次使用(๑˃̵ᴗ˂̵)و");
+    }
+
+
+
+}
