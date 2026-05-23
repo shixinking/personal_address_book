9d74d60486ca4f46ceb909dc2b96970cb3f03172
start <2105493561@qq.com>
2026-05-23 17:05:17 +0800

    完成主函数

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,49 @@
-import entity.Person;
+import manage.PersonManage;
+import util.ScannerUtil;
+import view.MainMenu;
 
 public class Main {
     static void main() {
-        Person person = new Person();
-        person.setGender("男");
-        person.get_info();
+        while (true)
+        {
+            boolean f = false;
+            MainMenu.show();
+            int num;
+            num = ScannerUtil.getMenuChoice("请输入:",0,6);
+            switch (num){
+                case 0:
+                    PersonManage.showPersonAddress();
+                    break;
+                case 1:
+                    PersonManage.selectPerson();
+                    break;
+                case 2:
+                    PersonManage.deletePerson();
+                    break;
+                case 3:
+                    PersonManage.addPerson();
+                    break;
+                case 4:
+                    PersonManage.updatePerson();
+                    break;
+                case 5:
+                    PersonManage.recreateAddress();
+                    break;
+                case 6:
+                    MainMenu.exit();
+                    f = true;
+                    break;
+                default:
+                    break;
+            }
+
+            if (f)
+            {
+                break;
+            }
+        }
+
+        ScannerUtil.close();
     }
+
 }
